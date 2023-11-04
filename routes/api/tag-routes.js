b2c30470b785bb9cad47fc9004ab77e591e0ac95
Andrew Hall <andrewchall92@gmail.com>
2023-11-03 20:44:29 -0700

    I added the logic for the 'update tag' and 'delete tag' routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,14 +37,44 @@ router.post("/", async (req, res) => {
   } catch (error) {
     res.status(400).json(error);
   }
-});
+})
 
-router.put("/:id", (req, res) => {
+
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    
+    })
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id!"});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+
+    if(!tagData) {
+      res.status(404).json({ message: "No tag found wiht that id!"});
+    }
+    res.status(200).json({ message: "Tag deleted!"})
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
